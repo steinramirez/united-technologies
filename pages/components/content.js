3ffc6d69b00489599f669224ada35c30b767f42d
Stein Ramirez <steinvzla@gmail.com>
2023-02-09 18:50:54 -0500

    First commitment on United Technologies project website
    First commitment on UT project, builded on Next.js prior ui design and form functions

---
+++ pages/components/content.js
@@ -0,0 +1,56 @@
+
+
+export default function content(){
+  return (<><div className="h-content flex items-center justify-center text-center bg-united">
+    <p className="text-6xl text-aqua">United Technologies</p></div>
+    <div className=" flex items-left justify-center text-center bg-white">
+     <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h2 class="text-xs text-yellow-500 tracking-widest font-medium title-font mb-1">UNISERVICES</h2>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">United Technologies Agency </h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">"Bienvenido a nuestra Agencia Digital Disruptiva, donde llevamos tu negocio al siguiente nivel a través de soluciones innovadoras y efectivas en marketing digital. Con un equipo altamente capacitado y un enfoque en los resultados, te brindamos una presencia en línea sólida y una estrategia digital a la medida. Juntos, trabajaremos para aumentar tu visibilidad, generar más tráfico y convertir más prospectos en clientes. ¡Transformemos tu negocio juntos!"</p>
+    </div>
+    <div class="flex flex-wrap">
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Desarrollo de sitios web</h2>
+        <p class="leading-relaxed text-base mb-4">Creación de sitios web personalizados y responsivos que se adapten a cualquier dispositivo.</p>
+        <a class="text-yellow-500 inline-flex items-center">Más Info
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Marketing digital</h2>
+        <p class="leading-relaxed text-base mb-4">Campañas de publicidad en línea, SEO, marketing de contenidos y análisis de datos para mejorar la visibilidad en línea.</p>
+        <a class="text-yellow-500 inline-flex items-center">Más Info
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">E-commerce</h2>
+        <p class="leading-relaxed text-base mb-4">Desarrollo de tiendas en línea y soluciones de comercio electrónico para vender productos o servicios en línea.</p>
+        <a class="text-yellow-500 inline-flex items-center">Más Info
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Consultoría digital</h2>
+        <p class="leading-relaxed text-base mb-4">Asesoramiento en estrategias digitales para mejorar la presencia en línea y alcanzar objetivos de negocio.</p>
+        <a class="text-yellow-500 inline-flex items-center">Más Info
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+  </div>
+</section>
+    </div></>)
+}
