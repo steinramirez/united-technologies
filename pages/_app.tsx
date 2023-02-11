5cef996b35586112af4bd2f5d13323e4d931a670
Stein Ramirez <steinvzla@gmail.com>
2023-02-11 15:12:01 -0500

    tawk chat fixes , checking display on vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,7 @@ import Script from "next/script"
 export default function App({ Component, pageProps }) {
   return (
   <>
-  <Component {...pageProps} />
-  <Script id ="tawk" strategy="lazyOnload">
-    {`
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5880d66dbcf30e71ac11daaa/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-    `}
-  </Script>
+  <Component {...pageProps} /> 
   </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '../pages/components/nav'
 import Content from '../pages/components/content'
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react'
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -20,6 +23,11 @@ export default function Home() {
         <div>
           <Navbar />
           <Content />
+          <div className="App">
+            <TawkMessengerReact
+                propertyId="5880d66dbcf30e71ac11daaa"
+                widgetId="default"/>
+        </div>
         </div>
       </main>
       
